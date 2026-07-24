1fff6adf1c176add546b4351e17084feb0a04534
Ahmed Banakhar <ahmedbanakhar47@gmail.com>
2026-07-24 18:39:10 +0300

    .

--- lib/get_climate_cubit/get_climate_cubit.dart
+++ lib/get_climate_cubit/get_climate_cubit.dart
@@ -2,15 +2,14 @@ import 'dart:developer';
 
 import 'package:climate/get_climate_cubit/get_climate_states.dart';
 import 'package:climate/model/climate_model.dart';
-import 'package:climate/service/climate_exception.dart';
 import 'package:climate/service/climate_services.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class GetClimateCubit extends Cubit<ClimateState> {
   GetClimateCubit({ClimateServices? climateServices})
-      : _climateServices = climateServices ?? ClimateServices(Dio()),
-        super(NoClimateState());
+    : _climateServices = climateServices ?? ClimateServices(Dio()),
+      super(NoClimateState());
 
   final ClimateServices _climateServices;
 
